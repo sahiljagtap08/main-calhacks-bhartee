1c6531179c39c6872e9dce7d0ed06afd63e57013
Sahil Jagtap <sahiljagtap@Sahils-MacBook-Air-2.local>
2024-10-19 18:25:30 -0700

    fixed rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="bharteeai",
-    api_url="https://prototype.bhartee.ai",
-    db_url="sqlite:///production.db",
+    db_url="sahil-93cc5:fEzPgK4gGQqG2CbPP8PxtvBNbIgRoR9A@svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com:3333",
+    api_url="https://prototype.bhartee.ai",  # Update this with your actual domain
     env=rx.Env.PROD,
 )
